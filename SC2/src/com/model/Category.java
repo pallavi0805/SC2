8f816662844ffa87dc2f2929fa04e7427da3c5fe
pallavi <pallavijswl502@gmail.com>
2016-09-17 20:06:13 +0530

    Update Category.java

--- SC2/src/com/model/Category.java
+++ SC2/src/com/model/Category.java
@@ -13,7 +13,7 @@ public class Category {
 	public int getCategoryId() {
 		return categoryId;
 	}
-	public void setCategoryId(int catego) {
+	public void setCategoryId(int categorrrrrrrrrrrrrrrrrrrry) {
 		this.categoryId = categoryId;
 	}
 	public String getCategoryName() {
